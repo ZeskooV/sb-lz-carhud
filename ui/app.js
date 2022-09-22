3037b425b6940089506c74691e220856bec15ea5
Zesk García Rodrígues <62112491+ZeskooV@users.noreply.github.com>
2022-09-22 11:19:10 +0200

    Update app.js

--- ui/app.js
+++ ui/app.js
@@ -4,11 +4,15 @@ $(function(){
         let e = event.data;
 
         if (e.action === "InVeh"){ // si la accion es InVeh entonces...
-            $('.cuadro').show(); // mostrar carhud
-            $('.velocidadact').text(Math.round(e.kmh) + ""); // cambiar el texto de velocidadact por el mensaje evento kmh
-            $('.gasolinact').text(Math.round(e.fuel) + "%");  // cambiar el texto de gasolinact por el mensaje evento fuel, y poner % al final
-            $('.marchact').text(e.gear); // cambiar el texto de marchact por el mensaje evento gear, y poner
-            $('.dañoact').text(Math.round(e.salud / 10) + "%"); // cambiar el texto de dañoact por el mensaje evento damage, y poner % al final
+            if (e.showIng) {
+                $('.cuadro').show(); // mostrar carhud
+                $('.velocidadact').text(Math.round(e.kmh) + ""); // cambiar el texto de velocidadact por el mensaje evento kmh
+                $('.gasolinact').text(Math.round(e.fuel) + "%");  // cambiar el texto de gasolinact por el mensaje evento fuel, y poner % al final
+                $('.marchact').text(e.gear); // cambiar el texto de marchact por el mensaje evento gear, y poner
+                $('.dañoact').text(Math.round(e.salud / 10) + "%"); // cambiar el texto de dañoact por el mensaje evento damage, y poner % al final
+            }else{
+                $('.cuadro').hide();   
+            }
         } else if (e.action === "outVeh"){ // si la accion es outVeh entonces...
             $('.cuadro').hide(); // ocultar carhud
         } // fin
